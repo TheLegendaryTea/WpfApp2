34fc6e309f8c5b363b5bb4b40bdaf7f6cd8a2794
PC <PC@DESKTOP-VH0UJ4V>
2022-06-14 10:55:18 +0200

    Добавьте файлы проекта.

---
+++ WpfApp2/Tangens.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class Tangens
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Tangens(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double Tang()
+        {
+            return Y / X;
+        }
+    }
+}
